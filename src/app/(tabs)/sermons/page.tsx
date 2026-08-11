8edbb2dceed4d448137e317b255fa8a1035fdcb9
eunchan <taegwon02@gmail.com>
2026-08-11 13:59:07 +0900

    [Feat] Suspense 감싸기 (#9)

--- src/app/(tabs)/sermons/page.tsx
+++ src/app/(tabs)/sermons/page.tsx
@@ -1,7 +1,12 @@
 import SermonTabsClient from "@/components/sermon/SermonTabsClient";
 import { getSermons } from "@/lib/actions/sermon/sermons";
+import { Suspense } from "react";
 
 export default async function SermonsPage() {
   const sermons = await getSermons();
-  return <SermonTabsClient sermons={sermons} />;
+  return (
+    <Suspense fallback={null}>
+      <SermonTabsClient sermons={sermons} />
+    </Suspense>
+  );
 }
